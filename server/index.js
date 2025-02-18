f9e90e5704bae1a3cdced8dfe86236c34504ad07
GosuStely <stely.tishelov@gmail.com>
2025-02-18 16:26:56 +0100

    vercel.json

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use("/api", requestLogger);
 app.use("/api/likes", LikesRouter);
